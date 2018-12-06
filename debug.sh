209e4aaa1acbeca2606fd0cf650a6179f06efb74
djzhang <djzhang@djzhangs-MacBook-Pro.local>
2018-12-06 15:43:00 +0800

    global_npm

--- debug.sh
+++ debug.sh
@@ -56,3 +56,9 @@ then
 else
     echo "Exist. [trujunzhang-workspace]..."
 fi
+
+
+echo ""
+echo "This script is about to run npm."
+sh ./USERS/NODE.js/global_npm.sh
+echo "This script has just run npm."
