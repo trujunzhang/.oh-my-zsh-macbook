ecb35de83df25b95d977e99856a57a0c42d5a8fb
djzhang <djzhang@djzhangs-MacBook-Pro.local>
2018-12-20 19:27:04 +0800

    reinstallSftpDocker

---
+++ USERS/VPS/reinstallSftpDocker.sh
@@ -0,0 +1,19 @@
+docker run \
+-v /host/upload:/home/foo/upload \
+-p 2222:22 -d atmoz/sftp \
+foo:pass:1001
+
+
+# The below steps must be followed while creating Users for sftp access.
+# Create user john with ftpaccess group and /usr/bin/nologin shell.
+sudo useradd -m foo -g ftpaccess -s
+
+# Passwd the user.
+sudo passwd foo
+
+# Change ownership for the home directory.
+sudo chown root /home/foo
+
+# Create a folder inside home directory for writing and change ownership of that folder.
+sudo mkdir /home/foo/upload
+sudo chown foo:ftpaccess /home/foo/upload
