929e7c74789b558cc98b093445d19d38ecd73eda
djzhang <trujunzhang@gmail.com>
2019-01-21 19:52:40 +0800

    debug.

--- debug.sh
+++ debug.sh
@@ -1,61 +1,16 @@
 #!/usr/bin/env bash
 
-# submodule
-echo "First of all, pull all submodules. [spacemacs]"
-#git submodule init
-#git submodule update
+source ./bash/files-functions.sh
 
-echo ""
-# .oh-my-zsh(plugins)
-if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/trujunzhang" ]
-then
-    ln -s "$HOME/.oh-my-zsh-macbook/custom/plugins/trujunzhang"  "$HOME/.oh-my-zsh/custom/plugins/trujunzhang"
-    echo "File does not exist. [trujunzhang, zsh plugin]..."
-else
-    echo "Exist. [trujunzhang, zsh's plugin]..."
-fi
+directoryLink  "oh-my-zsh(plugin:trujunzhang)"  "$HOME/.oh-my-zsh-macbook/custom/plugins/trujunzhang"  "$HOME/.oh-my-zsh/custom/plugins/trujunzhang"
 
-echo ""
-# .oh-my-zsh
-if [ ! -f $HOME/.zshrc ]
-then
-    ln -s $HOME/.oh-my-zsh-macbook/zshrc.used $HOME/.zshrc
-    echo "File does not exist. [.zshrc]..."
-else
-    echo "Exist. [.zshrc]..."
-fi
+fileLink ".zshrc"  "$HOME/.oh-my-zsh-macbook/zshrc.used" "$HOME/.zshrc"
 
-echo ""
-# dotenv(Express.js environment variables)
-if [ ! -d $HOME/.dotenv ]
-then
-    ln -s $HOME/.oh-my-zsh-macbook/dotenv $HOME/.dotenv
-    echo "File does not exist. [.dotenv]..."
-else
-    echo "Exist. [.dotenv]..."
-fi
+directoryLink  "dotenv(Express.js environment variables)"  "$HOME/.oh-my-zsh-macbook/dotenv" "$HOME/.dotenv"
 
+fileLink ".spacemacs" "$HOME/.oh-my-zsh-macbook/spacemacs.used" "$HOME/.spacemacs"
 
-echo ""
-# spacemacs
-if [ ! -f $HOME/.spacemacs ]
-then
-    ln -s $HOME/.oh-my-zsh-macbook/spacemacs.used $HOME/.spacemacs
-    echo "File does not exist. [.spacemacs]..."
-else
-    echo "Exist. [.spacemacs]..."
-fi
-
-
-echo ""
-# my emacs layers
-if [ ! -d $HOME/.emacs.d/private/trujunzhang-workspace ]
-then
-    ln -s $HOME/.oh-my-zsh-macbook/emacs.d/layers/trujunzhang-workspace  $HOME/.emacs.d/private/trujunzhang-workspace
-    echo "File does not exist. [trujunzhang-workspace]..."
-else
-    echo "Exist. [trujunzhang-workspace]..."
-fi
+directoryLink "emacs(Private Layout)" "$HOME/.oh-my-zsh-macbook/emacs.d/layers/trujunzhang-workspace"  "$HOME/.emacs.d/private/trujunzhang-workspace"
 
 
 echo ""
