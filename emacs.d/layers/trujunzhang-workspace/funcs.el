46d7ee919071f293d1079586a693a4c4ac7ba5bb
djzhang <trujunzhang@gmail.com>
2019-04-01 12:12:33 +0800

    custom typescript-mode.

--- emacs.d/layers/trujunzhang-workspace/funcs.el
+++ emacs.d/layers/trujunzhang-workspace/funcs.el
@@ -1,4 +1,21 @@
+(defun trujunzhang-workspace/my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 20)
 
+  ;; (setq web-mode-markup-indent-offset 20
+  ;;       web-mode-css-indent-offset 2
+  ;;       web-mode-code-indent-offset 2
+  ;;       web-mode-block-padding 2
+  ;;       web-mode-comment-style 2
+        
+  ;;       web-mode-enable-css-colorization t
+  ;;       web-mode-enable-auto-pairing t
+  ;;       web-mode-enable-comment-keywords t
+  ;;       web-mode-enable-current-element-highlight t
+  ;;       web-mode-enable-auto-indentation nil
+  ;;       )
+
+  )
 
 (defun trujunzhang-workspace/gui-default()
  (setq default-frame-alist
--- emacs.d/layers/trujunzhang-workspace/packages.el
+++ emacs.d/layers/trujunzhang-workspace/packages.el
@@ -28,48 +28,140 @@
 ;;   `trujunzhang-workspace/post-init-PACKAGE' to customize the package as it is loaded.
 
 ;;; Code:
+;;; Usefule:
+;;; https://gist.github.com/trujunzhang/77a7393cc83f45426030b3828651f980
+;;; https://gist.github.com/pratikborde/9dea191449f090e38378209b23d8eb30
+;;; https://privatebin.net/?95b71760170ac5b4#T0rnilKc9UUPhcZFJcCuN1iXtS9UIVKAmTPXrYleDRk=
+
 
 (defconst trujunzhang-workspace-packages
   '(
-    (prettier-js :fetcher github :repo "prettier/prettier-emacs")
-    projectile
+    ;;; For typescript
+    tide
     typescript-mode
+    web-mode
+    (prettier-js :fetcher github :repo "prettier/prettier-emacs")
+    ;;; For projects
+    projectile
+    ;;; For matlab
     ;; matlab-mode
     ))
 
-(defun trujunzhang-workspace/init-prettier-js ()
-  (message "info: [trujunzhang]: pre init prettier-js")
-  (use-package prettier-js
-  )
-  ;; (require 'prettier-js)
-  ;; (spacemacs|diminish prettier-js-mode " Ⓟ" " P")
+;;; tide
+;;; =================================
+(defun trujunzhang-workspace/init-tide ()
+  (use-package tide
+    :defer t
+    :commands (typescript/jump-to-type-def)
+    :init
+    (progn
+      (evilified-state-evilify tide-references-mode tide-references-mode-map
+        (kbd "C-k") 'tide-find-previous-reference
+        (kbd "C-j") 'tide-find-next-reference
+        (kbd "C-l") 'tide-goto-reference)
+      (add-hook 'typescript-mode-hook 'tide-setup)
+      ;; (push 'company-tide company-backends-typescript-mode)
+      )
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "mg" "goto")
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "mh" "help")
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "mn" "name")
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "mr" "rename")
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "mS" "server")
+      (spacemacs/declare-prefix-for-mode 'typescript-mode "ms" "send")
+
+      (defun typescript/jump-to-type-def()
+        (interactive)
+        (tide-jump-to-definition t))
+
+      (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
+        "gb" 'tide-jump-back
+        "gt" 'typescript/jump-to-type-def
+        "gu" 'tide-references
+        "hh" 'tide-documentation-at-point
+        "rr" 'tide-rename-symbol
+        "Sr" 'tide-restart-server))))
+
+;;; web-mode
+;;; =================================
+(defun trujunzhang-workspace/post-init-web-mode ()
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+  ;; FIXME -- this is not good!
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (and (buffer-file-name)
+                         (string-equal "tsx" (file-name-extension (buffer-file-name))))
+                (tide-setup)
+                (flycheck-mode +1)
+                (eldoc-mode +1)
+                (when (configuration-layer/package-usedp 'company)
+                  (company-mode-on))))
+            )
+  ;;; Format
+  (add-hook 'web-mode-hook  'trujunzhang-workspace/my-web-mode-hook)
+)
+
+;;; typescript-mode
+;;; =================================
+(defun trujunzhang-workspace/init-typescript-mode ()
+  (use-package typescript-mode
+    :defer t
+    :config
+    (setq typescript-indent-level 2)
+    (progn
+      ;; (when typescript-fmt-on-save
+        ;; (add-hook 'typescript-mode-hook 'typescript/fmt-before-save-hook))
+      (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
+        "="  'typescript/format
+        "sp" 'typescript/open-region-in-playground))))
+
+;;; Typescript-mode
+;;; =================================
+(defun trujunzhang-workspace/post-init-typescript-mode()
+  (message "info: [trujunzhang]: <post> typescript-mode")
+
+  ;;; Auto save buffers.
+  (add-hook 'focus-out-hook 'trujunzhang-workspace/sooheon-save-all)
+
+  ;; (add-hook 'js2-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
+  ;; (add-hook 'react-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
+  ;; (add-hook 'typescript-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
+
+  ;; (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
   )
 
+;;; Prettier-js
+;;; =================================
+(defun trujunzhang-workspace/init-prettier-js ()
+  (message "info: [trujunzhang]: <init> prettier-js")
+  (use-package prettier-js
+    :defer t)
+  )
+
+(defun trujunzhang-workspace/post-init-prettier-js ()
+  (message "info: [trujunzhang]: <post> prettier-js")
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (add-hook 'web-mode-hook 'prettier-js-mode)
+  (add-hook 'typescript-mode-hook 'prettier-js-mode)
+  )
+
+;;; Matlab-mode
+;;; =================================
 (defun trujunzhang-workspace/init-matlab-mode()
   (use-package matlab-mode
     :defer t)
   )
 
-(defun trujunzhang-workspace/pre-init()
-  (message "info: trujunzhang: pre init package")
-  )
-
-(defun trujunzhang-workspace/init()
-  "Auto-save on focus out"
-  (message "info: trujunzhang: init package")
-  )
-
-(defun trujunzhang-workspace/post-init()
-  (message "info: trujunzhang: post init package")
-  )
-
+;;; Projectile
+;;; =================================
 (defun trujunzhang-workspace/post-init-projectile()
+  (message "info: [trujunzhang]: <post> projectile!")
 
- ;;;  (trujunzhang-workspace/gui-default)
+  ;;;  (setq projectile-project-search-path '("/Users/djzhang/Desktop/upwork-projects/CURRENT/python-ieatta-universal" "/Users/djzhang/Desktop/upwork-projects/CURRENT/python-politicl-universal"))
 
-;;;  (setq projectile-project-search-path '("/Users/djzhang/Desktop/upwork-projects/CURRENT/python-ieatta-universal" "/Users/djzhang/Desktop/upwork-projects/CURRENT/python-politicl-universal"))
-
-  (message "info: [trujunzhang]: post init package for projectile!")
   ;; (setq projectile-enable-caching t)
 
   ;; (projectile-register-project-type 'yarn '("package.json")
@@ -79,19 +171,6 @@
                                     ;; :test-suffix ".test")
 
 )
-
-(defun trujunzhang-workspace/post-init-typescript-mode()
-  (message "info: [trujunzhang]: post init typescript-mode")
-
-  (add-hook 'focus-out-hook 'trujunzhang-workspace/sooheon-save-all)
-
-  (add-hook 'js2-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
-  (add-hook 'react-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
-  (add-hook 'typescript-mode-hook (lambda () (add-hook 'after-save-hook 'trujunzhang-workspace/prettier t t)))
-
-  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
-  )
 
 
 
