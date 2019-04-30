090106dc50c1299be3b4546d1be1a33c8de0fdd6
djzhang <trujunzhang@gmail.com>
2019-04-30 10:26:14 +0800

    add lisp.

---
+++ tutorials/__Emacs_lisp/basic/string.el
@@ -0,0 +1,10 @@
+
+(substring "abcdefg" 0 3)
+
+(setq fileName "TableList.el")
+
+(message "Y")
+(message "The name of this buffer is: %s." (buffer-name))
+(message "The name of this buffer is: %s." fileName)
+
+ 
---
+++ tutorials/__Emacs_lisp/basic/variables.el
@@ -0,0 +1,9 @@
+
+
+(defvar current-counter-value 0)
+
+(setq fileName "TableList.el")
+
+(message "some-variable is %s" current-counter-value )
+;; (message "some-variable is %s" fileName )
+
