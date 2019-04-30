0254ed542cd22797c505e47f996d41ffd1283ff4
djzhang <trujunzhang@gmail.com>
2019-04-30 19:59:16 +0800

    rutorrent

--- USERS/Docker/reinstallVPSDockers.sh
+++ USERS/Docker/reinstallVPSDockers.sh
@@ -17,7 +17,12 @@ docker pull hwdsl2/ipsec-vpn-server
 # Step4: Install vpn docker.
 docker run -d --name ipsec-vpn-server --env-file /root/.vpn_env --restart always -p 500:500/udp -p 4500:4500/udp -v /lib/modules:/lib/modules:ro  --privileged hwdsl2/ipsec-vpn-server
 
-cd oh-my-zsh-macbook
+cd /root/oh-my-zsh-macbook
 cd USERS/Docker/docker-pure-ftpd
+
+docker-compose up -d
+
+cd /root/oh-my-zsh-macbook
+cd USERS/Docker/docker-rutorrent
 
 docker-compose up -d
