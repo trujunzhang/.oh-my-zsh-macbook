ce2290d755fabd7d11f31d1902470896cdf99deb
djzhang <trujunzhang@gmail.com>
2019-06-11 06:12:39 +0800

    Docker

--- USERS/Docker/reinstallVPSDockers.sh
+++ USERS/Docker/reinstallVPSDockers.sh
@@ -1,3 +1,13 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-14-04
+# Step 1 — Installing Docker
+wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker $(whoami)
+
+# https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 
 #!/usr/bin/env bash
 
@@ -17,12 +27,12 @@ docker pull hwdsl2/ipsec-vpn-server
 # Step4: Install vpn docker.
 docker run -d --name ipsec-vpn-server --env-file /root/.vpn_env --restart always -p 500:500/udp -p 4500:4500/udp -v /lib/modules:/lib/modules:ro  --privileged hwdsl2/ipsec-vpn-server
 
-cd /root/oh-my-zsh-macbook
+cd /root/.oh-my-zsh-macbook
 cd USERS/Docker/docker-pure-ftpd
 
 docker-compose up -d
 
-cd /root/oh-my-zsh-macbook
+cd /root/.oh-my-zsh-macbook
 # cd USERS/Docker/docker-rutorrent
 cd USERS/Docker/qbittorrent
 
