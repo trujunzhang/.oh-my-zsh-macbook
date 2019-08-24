28f01d610c5710c27524a69e6a8753b0cfe807cf
trujunzhang <trujunzhang@gmail.com>
2019-08-24 16:18:41 +0800

    movie.

--- USERS/movie/merge.sh
+++ USERS/movie/merge.sh
@@ -1,3 +1,7 @@
 cat video1.avi video2.avi video3.avi video4.avi > output.avi
 mencoder -forceidx -oac copy -ovc copy output.avi -o output_final.avi;
 rm output.avi
+
+
+cat 01.avi 02.avi 03.avi 04.avi > output.avi
+mencoder -forceidx -oac copy -ovc copy output.avi -o output_final.avi;
