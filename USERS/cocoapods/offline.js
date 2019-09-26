a2b74ed69451350fa528cc46dd79f1aafb1643e2
trujunzhang <trujunzhang@gmail.com>
2019-09-26 11:33:22 +0800

    cocoapods.

--- USERS/cocoapods/offline.js
+++ USERS/cocoapods/offline.js
@@ -1,8 +1,7 @@
 const fs = require('fs')
-const { lstatSync, readdirSync } = require('fs')
-const { join } = require('path')
 
 let cocoapodsResposity = "/Users/djzhang/Documents/Organizations/IDES/cocoapods/.cocoapods/repos/master/Specs/"
+let offlineFolder = 'file:///Users/djzhang/Documents/Organizations/__CACHES/github/'
 let jsonFile = 'config/offline.json'
 
 function getDirectories(path) {
@@ -11,26 +10,48 @@ function getDirectories(path) {
     });
 }
 
-function replaceToOffline(line) {
+function replaceToOffline(obj, line) {
+    const last = obj['source']['git']
+    // console.log('last: ' + last)
+    if (last === line['git']) {
+        obj['source']['git'] = offlineFolder + line['folder']
+    }
+    return obj;
+}
+
+function offlineJsonFile(destFile, line) {
+
+    try {
+        var obj = JSON.parse(fs.readFileSync(destFile, 'utf8'))
+        const offlineObj = replaceToOffline(obj, line)
+        const content = JSON.stringify(offlineObj, null, 4)
+
+        // console.log('content: ' + content)
+        fs.writeFileSync(destFile, content);
+    } catch (e) {
+        console.log("Error: offline file, ", pkgFile)
+        // throw e
+    }
+}
+
+function replaceEachFiles(line) {
     let localPath = cocoapodsResposity + line['path']
     // console.log('localPath: '+ localPath )
 
     var folders = getDirectories(localPath)
     for (var i = 0; i < folders.length; i++) {
-        var destPath = localPath + "/" + folders[i]
+        var destPath = localPath + "/" + folders[i] + "/" + line['filename'] + ".podspec.json"
+        offlineJsonFile(destPath, line)
         console.log('destPath: ' + destPath)
     }
 }
 
-function replaceEachLine(localPath, line) {
-
-}
-
 try {
     var obj = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
-    var line = obj[2]
+    // var line = obj[2]
+    var line = obj[obj.length - 1]
 
-    replaceToOffline(line)
+    replaceEachFiles(line)
     // console.log('djzhang, ' + line )
 } catch (e) {
     console.log("Error: restore build.current:", jsonFile)
