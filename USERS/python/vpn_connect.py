c008304e1fc145b84fa8d16dcdd65ad12e255bb1
djzhang <djzhang@djzhangs.local>
2019-11-25 18:41:56 +0800

    vpn.

--- USERS/python/vpn_connect.py
+++ USERS/python/vpn_connect.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 import threading
 
+# https://stackoverflow.com/questions/42499299/python-threading-multiple-while-true-loops
 while True:
     subprocess.call(["networksetup","-connectpppoeservice","VPN (L2TP)"]) 
     time.sleep(15)
