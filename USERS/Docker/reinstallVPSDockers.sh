df3ef20f93ce7708ec1023aa881f001fcd101d06
trujunzhang <trujunzhang@gmail.com>
2020-08-02 19:14:40 +0800

    docker

--- USERS/Docker/reinstallVPSDockers.sh
+++ USERS/Docker/reinstallVPSDockers.sh
@@ -13,7 +13,7 @@
 
 # Important:
 #    First, load the IPsec af_key kernel module on the Docker host. This step is optional for Ubuntu and Debian.
-sudo modprobe af_key
+### sudo modprobe af_key
 
 # Step1: Remove docker ps.
 docker rm -f $(docker ps -a -q)
@@ -22,10 +22,10 @@ docker rm -f $(docker ps -a -q)
 # docker rmi $(docker images -q)
 
 # Step3: Install docker images.
-docker pull hwdsl2/ipsec-vpn-server
+### docker pull hwdsl2/ipsec-vpn-server
 
 # Step4: Install vpn docker.
-docker run -d --name ipsec-vpn-server --env-file /root/.oh-my-zsh-macbook/USERS/Docker/.vpn_env --restart always -p 500:500/udp -p 4500:4500/udp -v /lib/modules:/lib/modules:ro  --privileged hwdsl2/ipsec-vpn-server
+### docker run -d --name ipsec-vpn-server --env-file /root/.oh-my-zsh-macbook/USERS/Docker/.vpn_env --restart always -p 500:500/udp -p 4500:4500/udp -v /lib/modules:/lib/modules:ro  --privileged hwdsl2/ipsec-vpn-server
 
 cd /root/.oh-my-zsh-macbook
 cd USERS/Docker/docker-pure-ftpd
