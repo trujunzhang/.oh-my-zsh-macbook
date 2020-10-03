54e7b3ea12146d4f1acf9fa50065cf8cdb8bd8d2
djzhang <djzhang@djzhangs-MacBook-Pro.local>
2020-10-03 18:08:29 +0800

    os.

---
+++ djzhang-macos.mk
@@ -0,0 +1,31 @@
+# xcode
+    $ app store
+
+# oh-my-zsh
+    $ /Volumes/Data/ALLNew_Macbook/Users/djzhang/.oh-my-zsh
+
+#  .oh-my-zsh-macbook
+    $ ./allLinks.sh
+
+# homebrew
+    $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    $ brew install autojump gpg
+
+# rvn(ruby)
+    $ https://rvm.io/
+		$ rvm install 2.6.0
+		$ rvm use 2.6.0
+
+# cocoapods(ruby)
+    $ gem install cocoapods
+		$ pod setup
+		$ "/Users/djzhang/.cocoapods/repos/master/Specs/"
+
+# nvm(node)
+    $ dnvmreset
+
+# atom(ide)
+    $ /Users/djzhang/.atom
+
+# vscode(ide)
+    $ /Users/djzhang/.vscode
