3da35940b06ec84b175e9394a570e8f538866946
djzhang <trujunzhang@gmail.com>
2020-10-25 10:43:39 +0800

    add purcell emacs.d

---
+++ purcell/emacs.d/lisp/init-evil.el
@@ -0,0 +1,14 @@
+;;; init-evil.el --- evil-mode -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; See also init-evil.el.
+
+;;; Code:
+;;; https://github.com/emacs-evil/evil
+
+(require-package 'evil)
+(evil-mode 1)
+
+
+(provide 'init-evil)
+;;; init-evil.el ends here
