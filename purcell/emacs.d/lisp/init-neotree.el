61ea175bfb0014442ed87bd33df75cb615eb58d9
djzhang <trujunzhang@gmail.com>
2020-10-27 08:54:54 +0800

    neotree-project-dir

--- purcell/emacs.d/lisp/init-neotree.el
+++ purcell/emacs.d/lisp/init-neotree.el
@@ -15,5 +15,17 @@
 ; (global-set-key [f8] 'neotree-toggle)
 (define-key evil-normal-state-map "n" 'neotree-toggle)
 
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let* ((project-dir (ffip-get-project-root-directory))
+         (file-name (buffer-file-name)))
+    (if project-dir
+        (progn
+          (neotree-dir project-dir)
+          (neotree-find file-name))
+      (message "Could not find git project root."))))
+;; }}
+
 (provide 'init-neotree)
 ;;; init-neotree.el ends here
