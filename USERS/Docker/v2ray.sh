8134d82c6df00c2d18984b855b367bc73c948d3b
trujunzhang <trujunzhang@gmail.com>
2021-01-27 20:18:02 +0800

    installation

---
+++ USERS/Docker/v2ray.sh
@@ -0,0 +1,11 @@
+* installation
+    https://toutyrater.github.io/app/docker-deploy-v2ray.html
+
+
+docker run \
+    --restart=always \
+    -d --name v2ray \ 
+    -v /etc/v2ray:/etc/v2ray \ 
+    -p 8888:8888 v2ray/official  v2ray \ 
+    -config=/etc/v2ray/config.json
+
