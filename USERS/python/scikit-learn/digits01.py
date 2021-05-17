465f5a21dc7e2df4f226dbb5bde50fbea9f3209d
djzhang <trujunzhang@gmail.com>
2021-05-17 15:08:36 +0800

    python.

---
+++ USERS/python/scikit-learn/digits01.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+digits = load_digits()
+
+print(digits.data.shape)
+
+
+pca = PCA(2)  # project from 64 to 2 dimensions
+projected = pca.fit_transform(digits.data)
+print(digits.data.shape)
+print(projected.shape)
+
+plt.scatter(projected[:, 0], projected[:, 1],
+            c=digits.target, edgecolor='none', alpha=0.5,
+            cmap=plt.cm.get_cmap('Accent', 10))
+plt.xlabel('component 1')
+plt.ylabel('component 2')
+plt.colorbar()
+
+plt.show()
