a2d0ec3e3dc95aa54f1326b12366ff8678074f92
djzhang <trujunzhang@gmail.com>
2021-08-14 18:17:03 +0800

    v2ray multiple ports.

--- USERS/Docker/v2ray.sh
+++ USERS/Docker/v2ray.sh
@@ -1,12 +1,15 @@
-* installation
-    https://toutyrater.github.io/app/docker-deploy-v2ray.html
+# installation
+#   https://toutyrater.github.io/app/docker-deploy-v2ray.html
 
 
 docker run \
     --restart=always \
     -d --name v2ray \
     -v /etc/v2ray:/etc/v2ray \
-    -p 8888:8888 v2ray/official  v2ray \
+    -p 8888:8888
+    -p 6666:6666
+    -p 443:443
+    v2ray/official  v2ray \
     -config=/etc/v2ray/config.json
 
 
