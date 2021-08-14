03e8ea9e22c6e7e38b2ce54bc6b7916745882bd0
djzhang <trujunzhang@gmail.com>
2021-08-14 18:21:48 +0800

    v2ray.sh

--- USERS/Docker/v2ray.sh
+++ USERS/Docker/v2ray.sh
@@ -6,9 +6,9 @@ docker run \
     --restart=always \
     -d --name v2ray \
     -v /etc/v2ray:/etc/v2ray \
-    -p 8888:8888
-    -p 6666:6666
-    -p 443:443
+    -p 8888:8888 \
+    -p 6666:6666 \
+    -p 443:443 \
     v2ray/official  v2ray \
     -config=/etc/v2ray/config.json
 
