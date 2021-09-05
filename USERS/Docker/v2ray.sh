5d4c1229b8538f637663f312224c5c51456a2b91
djzhang <trujunzhang@gmail.com>
2021-09-05 17:42:56 +0800

    v2ray ports.

--- USERS/Docker/v2ray.sh
+++ USERS/Docker/v2ray.sh
@@ -6,9 +6,13 @@ docker run \
     --restart=always \
     -d --name v2ray \
     -v /etc/v2ray:/etc/v2ray \
-    -p 1234:1234\
-    -p 2345:2345\
-    -p 3456:3456\
+    -p 1111:1111\
+    -p 1112:1112\
+    -p 1113:1113\
+    -p 1114:1114\
+    -p 1115:1115\
+    -p 1116:1116\
+    -p 1117:1117\
     v2ray/official  v2ray \
     -config=/etc/v2ray/config.json
 
