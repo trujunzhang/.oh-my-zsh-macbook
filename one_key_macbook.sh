7016eecf495a98c1c967d1eba1c75555c553fd18
djzhang <trujunzhang@gmail.com>
2021-10-02 17:13:00 +0800

    macbook

--- one_key_macbook.sh
+++ one_key_macbook.sh
@@ -5,14 +5,27 @@
 #   .4 .oh-my-zsh
 #   .5 ./allLinks
 
+# cp ~/Desktop/zip/Homebrew /usr/local/Homebrew
+# cp ~/Desktop/zip/Homebrew /usr/local/Homebrew
 
 # Import function files.
 source ./bash/files-functions.sh
 source ./bash/tools.sh
 
-
-if !command_exists brew; then
+# homebrew
+if ! command_exists brew; then
     ( exec "./bash/homebrew_install.sh" )
+    brew install autojump gpg ssh-copy-id
+fi    
+
+# nvm(node.js)
+if [ ! -d  ~/.nvm ]; then
+    cd ~/ && rm -rf .nvm 
+    git clone file:///Users/djzhang/Documents/Organizations/IDES/nvm/localNVM .nvm
+    cd ~/.nvm  && git checkout v0.38.0 && . ~/.nvm/nvm.sh && nvm install 12.13.1 && nvm use 12.13.1
+    npm install -g yarn
+    npm install -g npm-check-updates
+    npm install -g monia-cli
 fi    
 
 
