5e04521207a6e3e60c8d21666a66e6a0d6368c4b
djzhang <trujunzhang@gmail.com>
2021-10-06 15:17:32 +0800

    youtube dl.

--- bash/yl.sh
+++ bash/yl.sh
@@ -4,4 +4,15 @@ start=$1
 end=$2
 list=$3
 
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --write-auto-sub --convert-subs=srt --playlist-start '$start' --playlist-end '$end' '$list'
+
+for i in {1..8}
+do
+    kill -9 $(pgrep youtube-dl)
+
+    youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' --write-auto-sub --convert-subs=srt --playlist-start $start --playlist-end $end $list
+
+    sleep 10s
+done
+
+
+
