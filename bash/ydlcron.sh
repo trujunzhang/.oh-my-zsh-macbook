b7d359aefb61d134b77eadd7cd662db0f97d77c0
djzhang <trujunzhang@gmail.com>
2021-10-13 19:16:28 +0800

    download.

--- bash/ydlcron.sh
+++ bash/ydlcron.sh
@@ -3,3 +3,10 @@ kill -9 $(pgrep youtube-dl)
 # 爱情万万岁 
 yl 25 30 https://www.youtube.com/watch?v=JRrp3b6ZOSM&list=PLRBClVey5Bqz6RG6d5Cc8QJsslWqFx8wG
 
+# 先结婚后恋爱
+yl 31 36 https://www.youtube.com/watch?v=-9MiSYjlrTg&list=PLjjPL6UFQG6qWNHrsD1IsrNK04dCp6zwt
+
+# 我在北京等你
+yl 19 24 https://www.youtube.com/watch?v=SPZZCqcRIJQ&list=PLQqbdnAgoRmZBTuONgIgF9Wkh_9GlTAS0
+
+
