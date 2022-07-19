18f4e4108d93769691c89399b4d52062bb83909f
djzhang <trujunzhang@gmail.com>
2022-07-19 17:01:15 +0800

    OpenSSL

--- oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
+++ oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
@@ -362,6 +362,7 @@ alias dfpython="cd /Users/djzhang/Documents/Organizations/TRUJUNZHANG/_oh-my-zsh
 alias drunvpn="cd /Users/djzhang/Documents/Organizations/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/python && python vpn_connect.py"
 alias drv="cd /Users/djzhang/Documents/Organizations/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/python && python vpn_connect.py"
 alias drunoffline="cd /Users/djzhang/Documents/Organizations/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/cocoapods && node offline.js"
+alias drunhttp='cd /Users/djzhang/Documents/Organizations/__CACHES/github && python3 -m http.server --cgi 8080'
 alias wcdxxx="mkdir -p /tmp/xxx && cd /tmp/xxx"
 alias wdwxxx="mkdir -p ~/Downloads/xxx && cd ~/Downloads/xxx"
 
