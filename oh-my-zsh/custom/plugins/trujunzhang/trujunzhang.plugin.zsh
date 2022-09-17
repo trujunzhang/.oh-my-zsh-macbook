1ac3ff0772e48a3468ea814edd4a25dab5c8ee69
djzhang <trujunzhang@gmail.com>
2022-09-17 09:07:52 +0800

    rn

--- oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
+++ oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
@@ -252,7 +252,8 @@ alias expandroidkeystore='expo fetch:android:keystore'
 
 # Expo(eas)
 alias easLogin='eas login'
-alias easBuildOnlineApkProduction='eas build --platform android --profile production --no-wait'
+alias easBuild_OnlineApk_Preview='eas build --platform android --profile preview --no-wait'
+alias easBuild_OnlineApk_Production='eas build --platform android --profile production --no-wait'
 alias easBuildLocalApkProduction='eas build --local --output bundletool/app_release.apk --platform android --profile production'
 alias easBuildIOS='eas build --platform ios'
 alias easBuildAll='eas build --platform all'
