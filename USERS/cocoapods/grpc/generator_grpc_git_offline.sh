46006948983f00589d6e3fa99ea1a53149a32b79
djzhang <trujunzhang@gmail.com>
2022-09-27 09:48:13 +0800

    envoy

--- USERS/cocoapods/grpc/generator_grpc_git_offline.sh
+++ USERS/cocoapods/grpc/generator_grpc_git_offline.sh
@@ -14,25 +14,25 @@ rm -f $END_TAG
 # ========================================================= 
 # clone
 # ========================================================= 
-	url = https://github.com/abseil/abseil-cpp.git
-	url = https://github.com/google/benchmark
-	url = https://github.com/google/bloaty.git
-	url = https://github.com/google/boringssl.git                         boringssl-with-bazel
-	url = https://github.com/c-ares/c-ares.git                            cares
-	url = https://github.com/envoyproxy/data-plane-api.git                envoy-api
-	url = https://github.com/googleapis/googleapis.git
-	url = https://github.com/google/googletest.git
-	url = https://github.com/libuv/libuv.git
-	url = https://github.com/census-instrumentation/opencensus-proto.git
-	url = https://github.com/open-telemetry/opentelemetry-proto.git       opentelemetry
-	url = https://github.com/protocolbuffers/protobuf.git
-	url = https://github.com/google/re2.git
-	url = https://github.com/cncf/xds.git
-	url = https://github.com/madler/zlib
+git clone https://github.com/abseil/abseil-cpp.git                        $OUT_GIT_FOLD/
+git clone https://github.com/google/benchmark                             $OUT_GIT_FOLD/
+git clone https://github.com/google/bloaty.git                            $OUT_GIT_FOLD/
+git clone https://github.com/google/boringssl.git                         $OUT_GIT_FOLD/boringssl-with-bazel
+git clone https://github.com/c-ares/c-ares.git                            $OUT_GIT_FOLD/cares
+git clone https://github.com/envoyproxy/data-plane-api.git                $OUT_GIT_FOLD/envoy-api
+git clone https://github.com/googleapis/googleapis.git                    $OUT_GIT_FOLD/
+git clone https://github.com/google/googletest.git                        $OUT_GIT_FOLD/
+git clone https://github.com/libuv/libuv.git                              $OUT_GIT_FOLD/ 
+git clone https://github.com/census-instrumentation/opencensus-proto.git  $OUT_GIT_FOLD/
+git clone https://github.com/open-telemetry/opentelemetry-proto.git       $OUT_GIT_FOLD/opentelemetry
+git clone https://github.com/protocolbuffers/protobuf.git                 $OUT_GIT_FOLD/
+git clone https://github.com/google/re2.git                               $OUT_GIT_FOLD/
+git clone https://github.com/cncf/xds.git                                 $OUT_GIT_FOLD/
+git clone https://github.com/madler/zlib                                  $OUT_GIT_FOLD/
 
 
 
 
-	# url = https://github.com/aquynh/capstone.git
-	# url = https://github.com/nico/demumble.git
+	# git clone https://github.com/aquynh/capstone.git
+	# git clone https://github.com/nico/demumble.git
 
