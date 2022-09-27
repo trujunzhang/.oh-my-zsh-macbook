69b0273ae83a2f4c2f6d8d01870ec618bc015715
djzhang <trujunzhang@gmail.com>
2022-09-27 09:49:41 +0800

    END_TAG

--- USERS/cocoapods/grpc/generator_grpc_git_offline.sh
+++ USERS/cocoapods/grpc/generator_grpc_git_offline.sh
@@ -14,21 +14,21 @@ rm -f $END_TAG
 # ========================================================= 
 # clone
 # ========================================================= 
-git clone https://github.com/abseil/abseil-cpp.git                        $OUT_GIT_FOLD/
-git clone https://github.com/google/benchmark                             $OUT_GIT_FOLD/
-git clone https://github.com/google/bloaty.git                            $OUT_GIT_FOLD/
+git clone https://github.com/abseil/abseil-cpp.git                        $OUT_GIT_FOLD/abseil-cpp
+git clone https://github.com/google/benchmark                             $OUT_GIT_FOLD/benchmark
+git clone https://github.com/google/bloaty.git                            $OUT_GIT_FOLD/bloaty
 git clone https://github.com/google/boringssl.git                         $OUT_GIT_FOLD/boringssl-with-bazel
 git clone https://github.com/c-ares/c-ares.git                            $OUT_GIT_FOLD/cares
 git clone https://github.com/envoyproxy/data-plane-api.git                $OUT_GIT_FOLD/envoy-api
-git clone https://github.com/googleapis/googleapis.git                    $OUT_GIT_FOLD/
-git clone https://github.com/google/googletest.git                        $OUT_GIT_FOLD/
-git clone https://github.com/libuv/libuv.git                              $OUT_GIT_FOLD/ 
-git clone https://github.com/census-instrumentation/opencensus-proto.git  $OUT_GIT_FOLD/
+git clone https://github.com/googleapis/googleapis.git                    $OUT_GIT_FOLD/googleapis
+git clone https://github.com/google/googletest.git                        $OUT_GIT_FOLD/googletest
+git clone https://github.com/libuv/libuv.git                              $OUT_GIT_FOLD/libuv 
+git clone https://github.com/census-instrumentation/opencensus-proto.git  $OUT_GIT_FOLD/opencensus-proto
 git clone https://github.com/open-telemetry/opentelemetry-proto.git       $OUT_GIT_FOLD/opentelemetry
-git clone https://github.com/protocolbuffers/protobuf.git                 $OUT_GIT_FOLD/
-git clone https://github.com/google/re2.git                               $OUT_GIT_FOLD/
-git clone https://github.com/cncf/xds.git                                 $OUT_GIT_FOLD/
-git clone https://github.com/madler/zlib                                  $OUT_GIT_FOLD/
+git clone https://github.com/protocolbuffers/protobuf.git                 $OUT_GIT_FOLD/protobuf
+git clone https://github.com/google/re2.git                               $OUT_GIT_FOLD/re2
+git clone https://github.com/cncf/xds.git                                 $OUT_GIT_FOLD/xds
+git clone https://github.com/madler/zlib                                  $OUT_GIT_FOLD/zlib
 
 
 
