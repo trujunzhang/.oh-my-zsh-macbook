9fc73450068123a68bc9d552251019aaaf6a0ce2
djzhang <trujunzhang@gmail.com>
2022-09-27 09:34:55 +0800

    git

--- USERS/cocoapods/grpc/generator_grpc_git_urls.sh
+++ USERS/cocoapods/grpc/generator_grpc_git_urls.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
-DIR='/Users/djzhang/Documents/Organizations/__CACHES/github/common'
-OUT_GIT_FILE='/Users/djzhang/Documents/Organizations/__CACHES/github/resposity.txt'
+DIR='/Users/djzhang/Documents/Organizations/__CACHES/github/@spec/_master/grpc/.git/modules'
+OUT_GIT_FILE='/tmp/xxx/grpc.txt'
 
 rm -f $OUT_GIT_FILE
-find "$DIR" -maxdepth 4 -mindepth 1 -type d | while read filepath; do
+find "$DIR" -maxdepth 4 -mindepth 1 -type f | while read filepath; do
     parentname="$(basename "$filepath")"
 
     # echo "$filepath"
     # echo "$parentname"
 
-    if [ "$parentname" = ".git" ]; then
-        # echo "$filepath"
-        resposityPath="$(dirname "$filepath")"
+    if [ "$parentname" = "config" ]; then
+        echo "$filepath"
+
+        CFG_FILE="$filepath"
+        CFG_CONTENT=$(cat $CFG_FILE | sed -r '/[^=]+=[^=]+/!d' | sed -r 's/\s+=\s/=/g')
+        echo "$CFG_CONTENT" >> $OUT_GIT_FILE
+
+        # resposityPath="$(dirname "$filepath")"
         # echo "$resposityPath"
 
-        cd "$resposityPath"
-        git config --get remote.origin.url >> $OUT_GIT_FILE
+        # cd "$resposityPath"
+        # git config --get remote.origin.url >> $OUT_GIT_FILE
     fi
 done
