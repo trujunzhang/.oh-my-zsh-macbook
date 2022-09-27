dfcbf221bcf19bd1bc27a9a989f0db26a8256d8d
djzhang <trujunzhang@gmail.com>
2022-09-27 18:53:14 +0800

    GRPC_BRANCH_ZIP_FILE

--- USERS/cocoapods/grpc/git_clone_grpc_branch.sh
+++ USERS/cocoapods/grpc/git_clone_grpc_branch.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 GRPC_GIT_URL="https://github.com/grpc/grpc.git"
-GRPC_BRANCH="V1.44.0"
+GRPC_BRANCH="v1.44.0"
 
 WORK_SPACE="/home/deploy/data/youtube"
+
+GRPC_ORIGINAL_LOCAL="file://$WORK_SPACE/grpc"
+GRPC_BRANCH_LOCAL="$WORK_SPACE/grpc-branch"
+
 GRPC_BRANCH_ZIP_FILE="$WORK_SPACE/grpc-branch.zip"
 
 git clone "$GRPC_GIT_URL" "$WORK_SPACE/grpc"
 
-git clone "$WORK_SPACE/grpc" "$WORK_SPACE/grpc-branch" --template= --single-branch --depth 1 --branch "$GRPC_BRANCH"
+git clone "$GRPC_ORIGINAL_LOCAL" "$GRPC_BRANCH_LOCAL" --template= --single-branch --depth 1 --branch "$GRPC_BRANCH"
 
-zip -r "$GRPC_BRANCH_ZIP_FILE" "$WORK_SPACE/grpc-branch" 
+zip -r "$GRPC_BRANCH_ZIP_FILE" "$GRPC_BRANCH_LOCAL" 
