1a405751e828a59834633a50f67d65d5473bdf0c
djzhang <trujunzhang@gmail.com>
2022-10-02 20:28:31 +0800

    kodi

--- one_key_brew_cask.sh
+++ one_key_brew_cask.sh
@@ -6,3 +6,38 @@ CURRENT=`pwd`
 source ./bash/files-functions.sh
 source ./bash/tools.sh
 
+# https://formulae.brew.sh/cask/
+# https://github.com/buo/homebrew-cask-upgrade
+
+
+# =========================================
+# Web browse
+# =========================================
+if [ ! -d  "/Applications/Google Chrome.app" ]; then
+    brew install --cask google-chrome
+fi
+
+if [ ! -d  "/Applications/Vivaldi.app" ]; then
+    brew install --cask vivaldi
+fi
+
+if [ ! -d  "/Applications/Opera.app" ]; then
+    brew install --cask opera
+fi
+
+if [ ! -d  "/Applications/Firefox.app" ]; then
+    brew install --cask firefox
+fi
+
+if [ ! -d  "/Applications/Brave Browser.app" ]; then
+    brew install --cask brave-browser
+fi
+
+if [ ! -d  "/Applications/Microsoft Edge.app" ]; then
+    brew install --cask microsoft-edge
+fi
+
+# =========================================
+# apps
+# =========================================
+    brew install --cask kodi
