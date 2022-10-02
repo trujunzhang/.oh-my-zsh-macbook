a4a4485a09cafddd0e8c6ccd40d49d41c671fed6
djzhang <trujunzhang@gmail.com>
2022-10-02 19:14:13 +0800

    one_key_node

--- one_key_macbook.sh
+++ one_key_macbook.sh
@@ -13,33 +13,6 @@ CURRENT=`pwd`
 source ./bash/files-functions.sh
 source ./bash/tools.sh
 
-# nvm(node.js)
-if [ ! -d  ~/.nvm ]; then
-    cd ~/ && rm -rf .nvm 
-    git clone file:///Users/djzhang/Documents/Organizations/IDES/nvm/localNVM .nvm
-    cd ~/.nvm  && git checkout v0.39.0 && . ~/.nvm/nvm.sh && nvm install 16.16.0 && nvm use 16.16.0
-fi    
-
-## list
-if ! command_exists yarn; then
-    npm install --location=global yarn
-    npm install --location=global npm-check-updates
-    npm install --location=global monia-cli
-    npm install --location=global firebase-tools
-fi    
-
-## typescript
-if ! command_exists tsc; then
-    npm install --location=global typescript typescript-formatter tern js-beautify eslint 
-fi    
-
-# expo
-if ! command_exists expo; then
-    # https://docs.expo.dev/get-started/installation/
-    npm install --location=global expo-cli 
-    # https://docs.expo.dev/build/setup/
-    npm install --location=global eas-cli
-fi    
 
 # cocoapods
 if ! command_exists pod; then
---
+++ one_key_node.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Requirements
+#   .1 the lastest macos
+#   .2 vpn
+#   .3 xcode command line tools
+#   .4 .oh-my-zsh
+#   .5 ./allLinks
+
+CURRENT=`pwd` 
+
+# Import function files.
+source ./bash/files-functions.sh
+source ./bash/tools.sh
+
+# nvm(node.js)
+if [ ! -d  ~/.nvm ]; then
+    cd ~/ && rm -rf .nvm 
+    git clone file:///Users/djzhang/Documents/Organizations/IDES/nvm/localNVM .nvm
+    cd ~/.nvm  && git checkout v0.39.0 && . ~/.nvm/nvm.sh && nvm install 16.16.0 && nvm use 16.16.0
+fi    
+
+## list
+if ! command_exists yarn; then
+    npm install --location=global yarn
+    npm install --location=global npm-check-updates
+    npm install --location=global monia-cli
+    npm install --location=global firebase-tools
+fi    
+
+## typescript
+if ! command_exists tsc; then
+    npm install --location=global typescript typescript-formatter tern js-beautify eslint 
+fi    
+
+# expo
+if ! command_exists expo; then
+    # https://docs.expo.dev/get-started/installation/
+    npm install --location=global expo-cli 
+    # https://docs.expo.dev/build/setup/
+    npm install --location=global eas-cli
+fi    
