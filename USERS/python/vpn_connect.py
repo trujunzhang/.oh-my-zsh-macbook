a1e1d9d3b71f2a5a4e87b098ebe66e3885dbab8c
djzhang <trujunzhang@gmail.com>
2022-10-27 21:33:58 +0800

    vpn

--- USERS/python/vpn_connect.py
+++ USERS/python/vpn_connect.py
@@ -2,10 +2,20 @@ import os
 import subprocess
 import time
 import threading
+import urllib.request
+
+
+def checkInternetUrllib(url='http://google.com', timeout=3):
+    try:
+        urllib.request.urlopen(url, timeout=timeout)
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 # https://stackoverflow.com/questions/42499299/python-threading-multiple-while-true-loops
-    
 subprocess.call(["networksetup","-disconnectpppoeservice","VPN (L2TP)"]) 
 while True:
-    subprocess.call(["networksetup","-connectpppoeservice","VPN (L2TP)"]) 
-    time.sleep(5)
+    if not checkInternetUrllib() :
+       subprocess.call(["networksetup","-connectpppoeservice","VPN (L2TP)"]) 
+    time.sleep(15)
--- oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
+++ oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
@@ -382,7 +382,7 @@ alias wsshlocal='ssh ubuntu@'
 alias wsshcopylocal='ssh-copy-id ubuntu@'
 # alias wsshtmp='ssh root@'
 # alias wsshcopytmp='ssh-copy-id root@'
-alias wsshtmp='ssh root@45.77.225.243'
+alias wsshtmp='ssh root@45.76.105.63'
 alias wsshcopytmp='ssh-copy-id root@'
 
 # Firebase
