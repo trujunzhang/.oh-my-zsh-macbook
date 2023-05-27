60df3fd65847526cdc9b1e68433da32091a5a747
djzhang <trujunzhang@gmail.com>
2023-05-27 23:00:16 +0800

    node

--- one_key_node.sh
+++ one_key_node.sh
@@ -6,40 +6,50 @@ CURRENT=`pwd`
 source ./bash/files-functions.sh
 source ./bash/tools.sh
 
+node_global="20.0.0"
+# node_global="18.12.0"
+node_old="16.16"
+
 # nvm(node.js)
 if [ ! -d  ~/.nvm ]; then
     cd ~/ && rm -rf .nvm 
     git clone file://$ORGANIZATIONS_HOME/IDES/nvm/localNVM .nvm
     cd ~/.nvm  && git checkout v0.39.3 && . ~/.nvm/nvm.sh 
-    nvm install 18.12.0
-    # nvm use 18.12.0 # default node.js
-    nvm alias default 18.12.0 # default node.js
-    nvm install 16.16
+    nvm install "$node_global"
+    nvm alias default "$node_global" # default node.js
+    nvm install "$node_old"
 fi    
 
+function install_global_npm {
     npm install --location=global prettier
 
-## list
-# if ! command_exists yarn; then
+    ## list
+    # if ! command_exists yarn; then
     npm install --location=global yarn
     npm install --location=global npm-check-updates
     npm install --location=global monia-cli
     npm install --location=global firebase-tools
-# fi    
+    # fi    
 
-## typescript
-# if ! command_exists tsc; then
+    ## typescript
+    # if ! command_exists tsc; then
     npm install --location=global typescript typescript-formatter tern js-beautify eslint 
-# fi    
+    # fi    
 
-# supabase
+    # supabase
     # npm install --location=global supabase
 
-# expo
-# if ! command_exists expo; then
+    # expo
+    # if ! command_exists expo; then
     # https://docs.expo.dev/get-started/installation/
     npm install --location=global expo-cli 
     # https://docs.expo.dev/build/setup/
     npm install --location=global eas-cli
-# fi    
+    # fi    
+}
 
+    nvm use "$node_global"
+    install_global_npm
+
+    nvm use "$node_old"
+    install_global_npm
