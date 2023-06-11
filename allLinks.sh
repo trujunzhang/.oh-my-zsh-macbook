c3aa7824a6e849e0967be8f3d815a487a8779914
djzhang <trujunzhang@gmail.com>
2023-06-11 16:44:19 +0800

    oh-my-zsh

--- allLinks.sh
+++ allLinks.sh
@@ -205,6 +205,11 @@ function link_symlink {
   # .oh-my-zsh
   # directoryLink  "oh-my-zsh(offline)"  "$OH_MY_ZSH_IDE_HOME"  "$HOME/.oh-my-zsh"
 
+
+  if [ -d "$HOME/.oh-my-zsh/custom/plugins/trujunzhang" ]; then
+      rm -rf "$HOME/.oh-my-zsh/custom/plugins/trujunzhang"
+  fi
+
   directoryLink  "oh-my-zsh(plugin:trujunzhang)"  "$TRUJUNZHANG_DOTFILES_HOME/oh-my-zsh/custom/plugins/trujunzhang"    "$HOME/.oh-my-zsh/custom/plugins/trujunzhang"
   # directoryLink  "oh-my-zsh(theme:powerlevel9k)"  "$POWER_LEVEL_9K_HOME"                                             "$HOME/.oh-my-zsh/custom/themes/powerlevel9k"
 
