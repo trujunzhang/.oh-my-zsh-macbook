43e02f329dcf962c64cfc2fa47f7d1f843ca44f2
djzhang <trujunzhang@gmail.com>
2023-06-13 09:25:38 +0800

    git_pull_nvm

--- bash/local_git_update.sh
+++ bash/local_git_update.sh
@@ -10,8 +10,25 @@ function git_pull_cocoapods {
     git reset --hard
     git pull
 
-    cd $ORGANIZATIONS_HOME/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/cocoapods && node offline.js
+    cd "$ORGANIZATIONS_HOME/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/cocoapods" && node offline.js
 }
 
+function git_pull_spacemacs {
+    cd "$ORGANIZATIONS_HOME/IDES/emacs-shim/spacemacs"
+    git pull
+}
+
+function git_pull_spacevim {
+    cd "$ORGANIZATIONS_HOME/IDES/vim-shim/SpaceVim"
+    git pull
+}
+
+function git_pull_nvm {
+    cd "$ORGANIZATIONS_HOME/IDES/nvm/localNVM"
+    git pull
+}
 
 git_pull_cocoapods
+git_pull_spacemacs
+git_pull_spacevim
+git_pull_nvm
