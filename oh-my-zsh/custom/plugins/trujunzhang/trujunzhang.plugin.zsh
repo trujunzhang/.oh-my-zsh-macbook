be7f1a6a97e1b4a4bdb87d628997c6af9c26928e
djzhang <trujunzhang@gmail.com>
2023-06-22 19:20:48 +0800

    emacs-plus

--- oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
+++ oh-my-zsh/custom/plugins/trujunzhang/trujunzhang.plugin.zsh
@@ -550,5 +550,6 @@ alias bitbucket_update_ssh="ssh-keygen -R bitbucket.org && curl https://bitbucke
 alias bitbucket_host_key="ssh git@bitbucket.org host_key_info"
 
 # homebrew
+# brew services start d12frosted/emacs-plus/emacs-plus@29
 alias binstall-emacs="brew tap d12frosted/emacs-plus && brew install emacs-plus@29 --with-spacemacs-icon && brew link emacs-plus@29"
 
