a25ebcc07d6bf64f99e9bab333d16c93ae94208c
djzhang <trujunzhang@gmail.com>
2023-06-30 09:39:55 +0800

    fish

--- one-key-fish.sh
+++ one-key-fish.sh
@@ -4,16 +4,32 @@
 # curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 fish_app=(
+    "jorgebucaran/fisher"
+
     "jethrokuan/z"
-
-    "PatrickF1/fzf.fish"
-
+    "patrickf1/fzf.fish"
+    "jorgebucaran/nvm.fish"
+    "budimanjojo/tmux.fish"
 )
 
 
 for (( i=0; i<${#fish_app[@]}; i++ ));
 do
     fisher install ${fish_app[$i]} 
+done
+
+
+omf_app=(
+    "brew"
+
+    "bobthefish"
+
+)
+
+
+for (( i=0; i<${#omf_app[@]}; i++ ));
+do
+    omf install ${omf_app[$i]} 
 done
 
 
