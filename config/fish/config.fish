a4eaf5447bff1884ab8f2e0cf32c5d9e51165ec3
djzhang <trujunzhang@gmail.com>
2023-06-30 14:46:21 +0800

    fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,10 +5,10 @@ end
 # Android SDK
 # ======================================================================
 # Add environment variable ANDROID_SDK_ROOT for cocos2d-x
-export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
-export ANDROID_USER_HOME=$HOME/Library/Android/sdk
-export PATH=$ANDROID_SDK_ROOT:$PATH
-export PATH=$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH
+# export ANDROID_SDK_ROOT=$HOME/Library/Android/sdk
+# export ANDROID_USER_HOME=$HOME/Library/Android/sdk
+# export PATH=$ANDROID_SDK_ROOT:$PATH
+# export PATH=$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH
 
 
 # Aliases
