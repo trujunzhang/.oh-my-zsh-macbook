3f21435b3464d789ede6296316709d22b7487040
djzhang <trujunzhang@gmail.com>
2023-07-11 10:12:21 +0800

    comment git menu

--- config/lvim/lua/user/whichkey.lua
+++ config/lvim/lua/user/whichkey.lua
@@ -197,32 +197,33 @@ local config = function()
       -- " Debug Adapter protocol:
       -- "   https://microsoft.github.io/debug-adapter-protocol/
       -- " Debugging
-      g = {
-        name = "Git",
-        g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
-        j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
-        k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
-        l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-        p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-        r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-        R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-        s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-        u = {
-          "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-          "Undo Stage Hunk",
-        },
-        o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-        b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-        c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-        C = {
-          "<cmd>Telescope git_bcommits<cr>",
-          "Checkout commit(for current file)",
-        },
-        d = {
-          "<cmd>Gitsigns diffthis HEAD<cr>",
-          "Git Diff",
-        },
-      },
+
+      -- g = {
+      --   name = "Git",
+      --   g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
+      --   j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+      --   k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+      --   l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+      --   p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+      --   r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+      --   R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+      --   s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+      --   u = {
+      --     "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+      --     "Undo Stage Hunk",
+      --   },
+      --   o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+      --   b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+      --   c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+      --   C = {
+      --     "<cmd>Telescope git_bcommits<cr>",
+      --     "Checkout commit(for current file)",
+      --   },
+      --   d = {
+      --     "<cmd>Gitsigns diffthis HEAD<cr>",
+      --     "Git Diff",
+      --   },
+      -- },
       l = {
         name = "LSP",
         a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
