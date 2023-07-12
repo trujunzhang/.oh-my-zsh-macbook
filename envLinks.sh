1a723b2eb2920d522064a839e75dcaf132dd615a
djzhang <trujunzhang@gmail.com>
2023-07-12 08:31:54 +0800

    env

---
+++ envLinks.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+CURRENT=`pwd`
+# Folders in Home.
+
+
+if [[ $(uname -m) == 'arm64' ]]; then
+    # echo M2
+    ORGANIZATIONS_HOME="$HOME/Documents/Organizations"  
+fi
+
+if [[ $(uname -m) == 'x86_64' ]]; then
+    # echo Mackook
+    ORGANIZATIONS_HOME="$HOME/Documents/Organizations"
+fi
+
+TRUJUNZHANG_DOTFILES_HOME="$ORGANIZATIONS_HOME/TRUJUNZHANG/_oh-my-zsh-macbook"
+
+
+echo "                         "
+echo "=================================================================================="
+echo "Bash version ${BASH_VERSION}...           "
+echo "                                          "
+echo "** CURRENT:                                "
+echo "$CURRENT"
+echo "** ORGANIZATIONS_HOME:                     "
+echo "$ORGANIZATIONS_HOME"
+echo "** TRUJUNZHANG_DOTFILES_HOME:              "
+echo "$TRUJUNZHANG_DOTFILES_HOME"
+echo "=================================================================================="
+echo "                                          "
+
+# Import function files.
+source ./bash/files-functions.sh
+
+WORKING_FOLD="$ORGANIZATIONS_HOME/__CODING/WORKING"
+ENV_PROJECT_FOLD="$WORKING_FOLD/projects_envs"
+
+envs_global_flutter_ieatta=(
+    "sbNext"     "packages/sbNext"
+)
+
+function link_envs {
+  source_name=$1 
+  dest_name=$2
+  envs_array=("$@")
+
+  fold_source="$ENV_PROJECT_FOLD/$source_name" 
+  fold_dest="$WORKING_FOLD/$dest_name"
+  
+
+echo "                         "
+echo "=================================================================================="
+echo "source fold: $fold_source"
+echo "dest   fold: $fold_dest"
+echo "=================================================================================="
+echo "                                          "
+
+# echo "${envs_array[*]}"
+
+for (( i=2; i<${#envs_array[@]}; i=i+2 ));
+do
+  project_env="${envs_array[$i+0]}"
+  project_dest="${envs_array[$i+1]}"
+
+  echo "element $i is ${project_env}"
+  echo "element $i is ${project_dest}"
+
+    
+  # fileLink  "env($env_project)"  "$source_fold/"
+done
+
+
+}
+
+link_envs "global-flutter-ieatta" "global-flutter-ieatta" "${envs_global_flutter_ieatta[@]}" 
+
+
+
+
+
