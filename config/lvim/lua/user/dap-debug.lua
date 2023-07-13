0440cacd60bd0d0650b031b5b2f69e65eb345362
djzhang <trujunzhang@gmail.com>
2023-07-13 17:56:42 +0800

    dap debug

--- config/lvim/lua/user/dap-debug.lua
+++ config/lvim/lua/user/dap-debug.lua
@@ -17,7 +17,8 @@ vim.keymap.set('n', '<leader>ui', require 'dapui'.toggle)
 
 require("dap-vscode-js").setup({
   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+  debugger_path = os.getenv('HOME') .. "/.local/share/lunarvim/site/pack/lazy/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
