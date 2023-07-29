e235027a766636905a00f9b9a2bf58e1b13f4902
djzhang <trujunzhang@gmail.com>
2023-07-29 08:58:28 +0800

    git update

--- bash/local_git_update.sh
+++ bash/local_git_update.sh
@@ -3,6 +3,7 @@
 # cocoapods
 
 function git_pull_cocoapods {
+    echo "element 0 is cocoapods"
     cd "$HOME/.cocoapods/repos/trunk"
 
     rm -rf "$HOME/.cocoapods/repos/trunk/Specs"
@@ -13,25 +14,22 @@ function git_pull_cocoapods {
     cd "$ORGANIZATIONS_HOME/TRUJUNZHANG/_oh-my-zsh-macbook/USERS/cocoapods" && node offline.js
 }
 
-function git_pull_spacemacs {
-    cd "$ORGANIZATIONS_HOME/IDES/emacs-shim/spacemacs"
-    git pull
-}
-
-function git_pull_spacevim {
-    cd "$ORGANIZATIONS_HOME/IDES/vim-shim/SpaceVim"
-    git pull
-}
-
-function git_pull_nvm {
-    cd "$ORGANIZATIONS_HOME/IDES/nvm/localNVM"
-    git pull
-}
+fold_apps=(
+    "$ORGANIZATIONS_HOME/IDES/emacs-shim/spacemacs"
+    "$ORGANIZATIONS_HOME/IDES/nvm/localNVM"
+    "$HOME/.local/share/lunarvim/lvim"
+    # "$HOME/.local/share/omf"
+  )
 
 if [ -d  "$HOME/.cocoapods/repos/trunk/Specs" ]; then
     git_pull_cocoapods
 fi
 
-git_pull_spacemacs
-git_pull_spacevim
-git_pull_nvm
+    for (( i=0; i<${#fold_apps[@]}; i=i+1 ));
+    do
+        echo "element $i is ${fold_apps[$i]}"
+        if [ -d  "${fold_apps[$i]}" ]; then
+           cd "${fold_apps[$i]}"
+           git pull
+        fi
+    done
