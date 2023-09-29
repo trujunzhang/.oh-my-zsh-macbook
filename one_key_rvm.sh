b1eab01be52afff555a87794b5f93e5747aa8bcc
djzhang <trujunzhang@gmail.com>
2023-09-29 11:26:21 +0800

    rvm

--- one_key_rvm.sh
+++ one_key_rvm.sh
@@ -1,3 +1,6 @@
+# https://rvm.io/rvm/install
+
+
 # As a first step install GPG keys used to verify installation package:
 
 gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -12,6 +15,10 @@ gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB8
 curl -L --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
 
 echo "rvm default" >> ~/.config/fish/config.fish
+
+
+rvm install 3.0.6
+
 
 
 
