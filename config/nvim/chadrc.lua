c69a4c386786c9b47be90d2c0b8320b967570c4f
djzhang <trujunzhang@gmail.com>
2023-11-08 21:24:06 +0800

    change nvchard theme to "one_light"

--- config/nvim/chadrc.lua
+++ config/nvim/chadrc.lua
@@ -6,8 +6,8 @@ local core = require "custom.utils.core"
 -- Path to overriding theme and highlights files
 local highlights = require "custom.highlights"
 M.ui = {
-  theme = "chadracula",
-  theme_toggle = { "chadracula", "one_light" },
+  theme = "one_light",
+  theme_toggle = { "one_light", "chadracula" },
   lsp_semantic_tokens = false,
   -- statusline = core.statusline,
   tabufline = core.tabufline,
@@ -35,7 +35,7 @@ M.ui = {
 
 M.settings = {
   cc_size = "130",
-  so_size = 10,
+  so_size = 16,
 
   -- Blacklisted files where cc and so must be disabled
   blacklist = {
