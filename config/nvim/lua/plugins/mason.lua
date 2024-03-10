a93987b643a4b736b363d40405f82a514ceb78f0
djzhang <trujunzhang@gmail.com>
2024-03-10 21:18:13 +0800

    mason

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -2,4 +2,8 @@
 return {
   "williamboman/mason.nvim",
   opts = {},
+  config = function(_, opts)
+    -- https://www.lazyvim.org/extras/formatting/prettier
+    table.insert(opts.ensure_installed, "prettier")
+  end,
 }
