8a054aef8c4869153394dd8ca66624632416191d
djzhang <trujunzhang@gmail.com>
2024-06-01 09:31:54 +0800

    feat: add 'telescope-git-file-history.nvim'

--- config/nvim-2kabhishek/lua/plugins/telescope/telescope.lua
+++ config/nvim-2kabhishek/lua/plugins/telescope/telescope.lua
@@ -1,18 +1,22 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
+        "nvim-telescope/telescope-symbols.nvim",
+        "molecule-man/telescope-menufacture",
+        "debugloop/telescope-undo.nvim",
+        {
+            "isak102/telescope-git-file-history.nvim",
+            dependencies = { "tpope/vim-fugitive" }
+        },
     },
-    "nvim-telescope/telescope-symbols.nvim",
-    "molecule-man/telescope-menufacture",
-    "debugloop/telescope-undo.nvim",
-  },
-  config = function()
-    require("plugins.telescope.config.telescope-config")
-  end,
-  cmd = "Telescope",
+    config = function()
+        require("plugins.telescope.config.telescope-config")
+    end,
+    cmd = "Telescope",
 }
