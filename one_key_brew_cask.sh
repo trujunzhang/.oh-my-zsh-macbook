7ff2705d1fdd45c27bab599d9e121435f32e0b6c
djzhang <trujunzhang@gmail.com>
2024-06-14 21:13:37 +0800

    feat: add 'Backup/Restore' MacOS applications

--- one_key_brew_cask.sh
+++ one_key_brew_cask.sh
@@ -67,7 +67,9 @@ brew_apps=(
 
 DEFAULTVALUE="install"
 Params="${1:-$DEFAULTVALUE}"
-echo "params = ${Params}"
+
+DEFAULBKKEY="local"
+ParamsBkKey="${2:-$DEFAULBKKEY}"
 
 app_backup_path_index() {
     case $1 in
@@ -82,13 +84,48 @@ hash_vals=("/Volumes/MacGame/APPS_BACKUP"
            "/Volumes/MacGame/"
            "/Volumes/MacUser/djzhang/Desktop/APPS_BACKUP");
 
-app_backup_path_index "ssd"
+app_backup_path_index ${ParamsBkKey}
 CURRENT_APP_BACKUP_PATH=${hash_vals[$?]}
-echo "current app backup path: ${CURRENT_APP_BACKUP_PATH}"
 
-function backupApps {
+PATH_BACKUP="${CURRENT_APP_BACKUP_PATH}/$(uname -m)/Applications"
+
+mkdir -p "${PATH_BACKUP}"
+
+info "==========================================================="
+info "params<brew-type> = ${Params}"
+info "params<Backup key> = ${ParamsBkKey}"
+info "current app backup fold: ${CURRENT_APP_BACKUP_PATH}"
+info "current app backup path: ${PATH_BACKUP}"
+info "==========================================================="
+
+function backup_apps {
     name=$1
-    app=$2
+
+    app_path_in_backup_folder="${PATH_BACKUP}/${name}"
+    info "Backup app: ${name}"
+    info "Backup app path: ${app_path_in_backup_folder}"
+
+    # if the app is already in backup folder, delete it first
+    if [ -d  "${app_path_in_backup_folder}" ]; then
+        info "** remove the old app: ${app_path_in_backup_folder}"
+        rm -rf "${app_path_in_backup_folder}"
+    fi
+    
+    setopt -s dotglob
+    cp -Rvp "/Applications/${name}" "${app_path_in_backup_folder}"
+}
+
+function restore_apps {
+    name=$1
+
+    app_path_in_backup_folder="${PATH_BACKUP}/${name}"
+    info "Restore app: ${name}"
+    info "Backup app path: ${app_path_in_backup_folder}"
+
+    if [ -d  "${app_path_in_backup_folder}" ]; then
+         setopt -s dotglob
+         cp -Rvp "${app_path_in_backup_folder}" "/Applications/${name}"
+    fi
 }
 
 function install_brew_app {
@@ -97,11 +134,11 @@ function install_brew_app {
 
     if [ "$Params" = "backup" ]; then
         if [ -d  "/Applications/${name}" ]; then
-            sudo rm -rf "/Applications/${name}"
+            backup_apps "${name}"
         fi
     elif [ "$Params" = "restore" ]; then
-        if [ -d  "/Applications/${name}" ]; then
-            sudo rm -rf "/Applications/${name}"
+        if [ ! -d  "/Applications/${name}" ]; then
+            restore_apps "${name}"
         fi
     elif [ "$Params" = "delete" ]; then
         if [ -d  "/Applications/${name}" ]; then
@@ -126,10 +163,13 @@ function install_brew_app {
 
 for (( i=0; i<${#brew_apps[@]}; i=i+2 ));
 do
-    echo "element $i is ${brew_apps[$i+0]}"
-    echo "element $i is ${brew_apps[$i+1]}"
+    info ""
+    info "element $i is ${brew_apps[$i+0]}"
+    info "element $i is ${brew_apps[$i+1]}"
 
+    info ""
     install_brew_app "${brew_apps[$i+0]}" "${brew_apps[$i+1]}"
+    info ""
 done
 
 if [[ $(uname -m) == 'x86_64' ]]; then
@@ -142,7 +182,7 @@ fi
 # apps
 # =========================================
 if [[ $(uname -m) == 'arm64' ]]; then
-    echo "install apps for arm64"
+    info "install apps for arm64"
     # install_brew_app "Docker.app" docker
 
     # install_brew_app "pgAdmin 4.app" pgadmin4
