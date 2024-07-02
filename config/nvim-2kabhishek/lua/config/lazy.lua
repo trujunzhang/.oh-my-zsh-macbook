b0eeee29b4e0b0efae2637c7e688ba05976572ac
djzhang <trujunzhang@gmail.com>
2024-07-02 17:45:13 +0800

    fixed: typescript plugin on the neovim

--- config/nvim-2kabhishek/lua/config/lazy.lua
+++ config/nvim-2kabhishek/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
--- config/nvim-2kabhishek/lua/plugins/javascript/lspsaga.lua
+++ config/nvim-2kabhishek/lua/plugins/javascript/lspsaga.lua
@@ -3,5 +3,10 @@ return {
   config = function()
     require("plugins.javascript.config.lspsaga-config")
   end,
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
   event = "LspAttach",
 }
