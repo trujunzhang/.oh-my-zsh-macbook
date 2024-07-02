77c5b5d3dfd1ac94e785b789ade9833865424cfe
djzhang <trujunzhang@gmail.com>
2024-07-02 10:08:59 +0800

    feat: add some 'LazyVim' plugins

--- config/nvim-2kabhishek/lua/config/lazy.lua
+++ config/nvim-2kabhishek/lua/config/lazy.lua
@@ -9,66 +9,67 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
-    spec = {
-        -- add LazyVim and import its plugins
-        { "LazyVim/LazyVim",                                     import = "lazyvim.plugins" },
-        -- import any extras modules here
-        { import = "lazyvim.plugins.extras.linting.eslint" },
-        { import = "lazyvim.plugins.extras.formatting.prettier" },
-        { import = "lazyvim.plugins.extras.lang.typescript" },
-        -- { import = "lazyvim.plugins.extras.lang.json" },
-        -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-        -- { import = "lazyvim.plugins.extras.lang.markdown" },
-        -- { import = "lazyvim.plugins.extras.lang.rust" },
-        -- { import = "lazyvim.plugins.extras.lang.tailwind" },
-        -- { import = "lazyvim.plugins.extras.coding.copilot" },
-        -- { import = "lazyvim.plugins.extras.dap.core" },
-        -- { import = "lazyvim.plugins.extras.vscode" },
-        -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-        -- { import = "lazyvim.plugins.extras.test.core" },
-        -- { import = "lazyvim.plugins.extras.coding.yanky" },
-        -- { import = "lazyvim.plugins.extras.editor.mini-files" },
-        -- { import = "lazyvim.plugins.extras.util.project" },
-        --  some LazyVim's disabled_plugins
-        -- { "folke/tokyonight.nvim", enabled = false },
-        { "stevearc/conform.nvim'",                              enabled = false },
-        { "folke/persistence.nvim",                              enabled = false },
-        -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-        -- import/override with your plugins
-        -- { import = "plugins" },
-        { import = "plugins.ui" },
-        { import = "plugins.language" },
-        { import = "plugins.tools" },
-        { import = "plugins.treesitter" },
-        { import = "plugins.lsp" },
-        { import = "plugins.completion" },
-        { import = "plugins.telescope" },
-        { import = "plugins.javascript" },
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.lang.markdown" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
+    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.vscode" },
+    -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    -- { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.coding.yanky" },
+    -- { import = "lazyvim.plugins.extras.editor.mini-files" },
+    -- { import = "lazyvim.plugins.extras.util.project" },
+    --  some LazyVim's disabled_plugins
+    -- { "folke/tokyonight.nvim", enabled = false },
+    -- { "stevearc/conform.nvim'",                              enabled = false },
+    -- { "folke/persistence.nvim",                              enabled = false },
+    -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    -- import/override with your plugins
+    { import = "plugins.ui" },
+    { import = "plugins.tools" },
+    { import = "plugins.completion" },
+    { import = "plugins.javascript" },
+    -- { import = "plugins.language" },
+    -- { import = "plugins.treesitter" },
+    -- { import = "plugins.lsp" },
+    -- { import = "plugins.telescope" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = false }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
-    defaults = {
-        -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-        -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-        lazy = false,
-        -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-        -- have outdated releases, which may break your Neovim install.
-        version = false, -- always use the latest git commit
-        -- version = "*", -- try installing the latest stable version for plugins that support semver
-    },
-    install = { colorscheme = { "tokyonight", "habamax" } },
-    checker = { enabled = false }, -- automatically check for plugin updates
-    performance = {
-        rtp = {
-            -- disable some rtp plugins
-            disabled_plugins = {
-                "gzip",
-                -- "matchit",
-                -- "matchparen",
-                -- "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
+  },
 })
