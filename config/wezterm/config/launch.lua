7ba9bc943f893ec14e3bede371e893ddfc039fd0
djzhang <trujunzhang@gmail.com>
2024-07-27 21:55:39 +0800

    feat: set 'launch' on the 'wezterm'

--- config/wezterm/config/launch.lua
+++ config/wezterm/config/launch.lua
@@ -5,19 +5,7 @@ local options = {
    launch_menu = {},
 }
 
-if platform.is_win then
-   options.default_prog = { 'pwsh' }
-   options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh' } },
-      { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      {
-         label = 'Git Bash',
-         args = { 'C:\\Users\\kevin\\scoop\\apps\\git\\current\\bin\\bash.exe' },
-      },
-   }
-elseif platform.is_mac_arm then
+if platform.is_mac_arm then
    options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
@@ -29,13 +17,6 @@ elseif platform.is_mac_x86 then
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
       { label = 'Fish', args = { '/usr/local/bin/fish', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
-   }
-elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
-   options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 end
