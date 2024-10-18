39152789853fd352404519b65774171f6d75ffbb
djzhang <trujunzhang@gmail.com>
2024-10-18 17:49:36 +0800

    feat: config on the 'GUI Singbox'

--- allLinks.sh
+++ allLinks.sh
@@ -346,6 +346,9 @@ function link_symlink {
     # Node.js
     # fileLink "Yarn(config)" "$TRUJUNZHANG_DOTFILES_HOME/config/yarn/.yarnrc" "$HOME/.yarnrc" "delete"
     directoryLink "npm(config)" "$TRUJUNZHANG_DOTFILES_HOME/config/npm" "$HOME/.config/npm" "delete"
+
+    # GUI.for.SingBox
+    directoryLink "SingBox(config)" "$TRUJUNZHANG_DOTFILES_HOME/config/GUI.for.SingBox" "/Users/djzhang/Library/Application Support/GUI.for.SingBox" "delete"
 }
 
 # function link_cocoapods {
