203deed75ee53b60068764ce079232cc9c99cf60
djzhang <trujunzhang@gmail.com>
2025-01-09 20:05:36 +0800

    feat: add 'Codeium' vpn

--- config/nvchad-Starter/lua/plugins/nvim-notify.lua
+++ config/nvchad-Starter/lua/plugins/nvim-notify.lua
@@ -1,32 +1,33 @@
 ---@type NvPluginSpec
 -- NOTE: Notification
 return {
-  "rcarriga/nvim-notify",
-  -- lazy = false,
-  event = "VeryLazy",
-  opts = {
-    level = 2,
-    minimum_width = 50,
-    render = "default",
-    stages = "fade_in_slide_out",
-    timeout = 3000,
-    top_down = false,
-  },
-  config = function(_, opts)
-    local base46 = require("nvconfig").base46
+    "rcarriga/nvim-notify",
+    -- lazy = false,
+    enabled = false,
+    event = "VeryLazy",
+    opts = {
+        level = 2,
+        minimum_width = 50,
+        render = "default",
+        stages = "fade_in_slide_out",
+        timeout = 3000,
+        top_down = false,
+    },
+    config = function(_, opts)
+        local base46 = require("nvconfig").base46
 
-    if base46.transparency then
-      opts.background_colour = "#000000"
-    end
+        if base46.transparency then
+            opts.background_colour = "#000000"
+        end
 
-    require("notify").setup(opts)
+        require("notify").setup(opts)
 
-    vim.notify = require "notify"
-    local messages = require "configs.messages"
-    math.randomseed(os.time())
-    local randomMessage = messages[math.random(#messages)]
-    if vim.g.startup_message then
-      vim.notify(randomMessage, vim.log.levels.INFO, { title = "Just For Fun:" })
-    end
-  end,
+        vim.notify = require "notify"
+        local messages = require "configs.messages"
+        math.randomseed(os.time())
+        local randomMessage = messages[math.random(#messages)]
+        if vim.g.startup_message then
+            vim.notify(randomMessage, vim.log.levels.INFO, { title = "Just For Fun:" })
+        end
+    end,
 }
