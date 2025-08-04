d139c71db82397ab93f72af30f5e53bd4d7ce28b
djzhang <trujunzhang@gmail.com>
2025-08-04 21:24:12 +0800

    feat: code 'conform'

--- config/neovim-linkarzu/lua/plugins/conform.lua
+++ config/neovim-linkarzu/lua/plugins/conform.lua
@@ -1,10 +1,10 @@
 local formatters_by_ft = {
     lua = { "stylua" },
     python = { "autopep8" },
-    cpp = { "clang_format" },
-    c = { "clang_format" },
-    go = { "gofumpt" },
-    cs = { "csharpier" },
+    -- cpp = { "clang_format" },
+    -- c = { "clang_format" },
+    -- go = { "gofumpt" },
+    -- cs = { "csharpier" },
     yaml = { "yamlfmt" },
     css = { "prettier" },
     flow = { "prettier" },
@@ -27,18 +27,18 @@ return {
     "stevearc/conform.nvim",
     event = "VeryLazy",
     opts = {
-        format_after_save = function(bufnr)
-            -- Disable with a global or buffer-local variable
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-            end
-            -- Disable autoformat for files in a certain path
-            local bufname = vim.api.nvim_buf_get_name(bufnr)
-            if bufname:match "/node_modules/" then
-                return
-            end
-            return { timeout_ms = 3000, lsp_format = "fallback" }
-        end,
+        -- format_after_save = function(bufnr)
+        --     -- Disable with a global or buffer-local variable
+        --     if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        --         return
+        --     end
+        --     -- Disable autoformat for files in a certain path
+        --     local bufname = vim.api.nvim_buf_get_name(bufnr)
+        --     if bufname:match "/node_modules/" then
+        --         return
+        --     end
+        --     return { timeout_ms = 3000, lsp_format = "fallback" }
+        -- end,
         formatters_by_ft = formatters_by_ft,
     },
 }
