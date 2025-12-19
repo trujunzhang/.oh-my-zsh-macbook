2ab4ff7c3d616baf05f2aa887310a0cbc63698e4
djzhang <trujunzhang@gmail.com>
2025-12-19 18:51:26 +0800

    feat: add 'MacOS_Restart'

---
+++ config/hammerspoon/applications/app_restart.lua
@@ -0,0 +1,21 @@
+function YesNoDialogBox(ActionFunc)
+    test = hs.chooser.new(ActionFunc)
+    test:rows(2)
+    test:choices({
+        { ["text"] = "Yes", ["subText"] = "", ["id"] = "yes" },
+        { ["text"] = "No", ["subText"] = "", ["id"] = "no" },
+    })
+    test:show()
+end
+function RebootIfChoice(input)
+    if input.id == "yes" then
+        hs.alert("Your choice was: yes")
+        hs.application.launchOrFocus("MacOS_Restart.app")
+    else
+        hs.alert("Your choice was: no")
+    end
+end
+
+hs.hotkey.bind({ "ctrl", "cmd" }, "r", function()
+    YesNoDialogBox(RebootIfChoice)
+end)
