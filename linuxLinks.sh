9b86923b19cfd817cde29d3335290f43d60bc88c
djzhang <trujunzhang@gmail.com>
2026-09-06 18:57:40 +0800

    feat: add 'linux zshenv'

---
+++ linuxLinks.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Import function files.
+source ./bash/files-functions.sh
+
+source ./bash/shellUtils.sh
+source ./bash/games/documents_fix.sh
+source ./bash/games/games-data.sh
+
+CURRENT=$(pwd)
+# Folders in Home.
+
+TRUJUNZHANG_DOTFILES_HOME="$CURRENT"
+
+show_info() {
+    info "                         "
+    info "=================================================================================="
+    info "Bash version ${BASH_VERSION}...           "
+    info "                                          "
+    info "** CURRENT:                         ${CURRENT}       "
+    info "** TRUJUNZHANG_DOTFILES_HOME:       ${TRUJUNZHANG_DOTFILES_HOME}       "
+    info "=================================================================================="
+    info "                                          "
+}
+
+link_common() {
+    # zshrc(rc)
+    fileLink ".zsh(env)" "$TRUJUNZHANG_DOTFILES_HOME/zshenv-linux.symlink" "$HOME/.zshenv"
+}
+
+link_common
+show_info
